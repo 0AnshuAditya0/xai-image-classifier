c882594dacde658049707108b15edb53d8cba35d
0AnshuAditya0 <anshubaka2004@gmail.com>
2025-11-25 19:03:33 +0530

    Fix: Remove /api prefix from routes for Hugging Face

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,3 @@
-#<<<<<<< HEAD
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,24 +17,3 @@ def get_db():
         yield db
     finally:
         db.close()
-=======
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./xai_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-#>>>>>>> 95fbb696c2bff2b497d717df118dc68761b9836f
